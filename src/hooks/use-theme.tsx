cce650704d761abc3f42f17206ff9aee3b304ad4
United Kingdom <ukserver150@gmail.com>
2025-10-12 20:01:25 +0000

    Try fixing this error: `Runtime Error: Error: useTheme must be used with

--- src/hooks/use-theme.tsx
+++ src/hooks/use-theme.tsx
@@ -1,19 +1,10 @@
 "use client"
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeProvider as NextThemesProvider, useTheme as useNextTheme } from "next-themes"
+import type { ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
 
-export const useTheme = () => {
-    const context = React.useContext(NextThemesProvider)
-  
-    if (context === undefined) {
-      throw new Error("useTheme must be used within a ThemeProvider")
-    }
-  
-    return context
-}
+export const useTheme = useNextTheme
