b1dbf6f9e96fec468737f449313df88989fedfc0
United Kingdom <ukserver150@gmail.com>
2025-10-12 18:56:50 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,14 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDyLrZ57cmxYD-hldLeqTRbXRMLLuuym7o",
+  authDomain: "call-me-a4977.firebaseapp.com",
+  databaseURL: "https://call-me-a4977-default-rtdb.firebaseio.com",
+  projectId: "call-me-a4977",
+  storageBucket: "call-me-a4977.appspot.com",
+  messagingSenderId: "46724772817",
+  appId: "1:46724772817:web:41fa71425f9e1579a9e6f0",
+  measurementId: "G-ZC4PL7FMB3"
 };
 
 function createFirebaseApp(config: FirebaseOptions) {
