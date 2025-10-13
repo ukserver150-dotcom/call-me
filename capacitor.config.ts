d0bfd9ff770e10715f6f75e2782713d49f0edfdb
United Kingdom <ukserver150@gmail.com>
2025-10-13 01:13:34 +0000

    npx cap init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/core';
+
+const config: CapacitorConfig = {
+  appId: 'com.echoverse.app',
+  appName: 'EchoVerse',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
